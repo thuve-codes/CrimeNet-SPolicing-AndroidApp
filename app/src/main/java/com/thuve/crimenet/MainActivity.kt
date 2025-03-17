13e8f83e105fa7c7d40035d54a79ba7f6ab48823
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-17 18:19:58 +0530

    Add: Getting Start page 0

--- app/src/main/java/com/thuve/crimenet/MainActivity.kt
+++ app/src/main/java/com/thuve/crimenet/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.thuve.crimenet
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.getting_start_1)
+        setContentView(R.layout.getting_start_0)
+        val btnsk1: Button = findViewById(R.id.skip_btn)
+        btnsk1.setOnClickListener {
+            setContentView(R.layout.getting_start_1)
+            val btnsk2: Button = findViewById(R.id.skip_button)
+            btnsk2.setOnClickListener {
+                setContentView(R.layout.getting_start_2)
+            }
+        }
     }
 }
