d1588230a1e56b9dc97163b69fb665a141bb9885
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-17 15:10:22 +0530

    Add: Getting Start page

--- app/src/main/java/com/thuve/crimenet/MainActivity.kt
+++ app/src/main/java/com/thuve/crimenet/MainActivity.kt
@@ -2,46 +2,10 @@ package com.thuve.crimenet
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.thuve.crimenet.ui.theme.CrimeNetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CrimeNetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CrimeNetTheme {
-        Greeting("Android")
+        setContentView(R.layout.getting_start_1)
     }
 }
