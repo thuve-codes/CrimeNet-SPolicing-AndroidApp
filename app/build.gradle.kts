e941a94a0c261c0779d19f42db4acedf4b450ecd
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-19 18:47:30 +0530

    WORKING: Activity-report_crime.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Google fonts dep
+
 }
