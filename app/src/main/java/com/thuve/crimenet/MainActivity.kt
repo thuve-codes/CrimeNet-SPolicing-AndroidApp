5abdf7e1006db682f1103de3ecd027f1fc521079
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-20 01:40:56 +0530

    Fixed: Activity-report_crime.xml

--- app/src/main/java/com/thuve/crimenet/MainActivity.kt
+++ app/src/main/java/com/thuve/crimenet/MainActivity.kt
@@ -2,7 +2,6 @@ package com.thuve.crimenet
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 
@@ -10,14 +9,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.getting_start_0)
-        val btnsk1: Button = findViewById(R.id.skip_btn)
-        btnsk1.setOnClickListener {
-            setContentView(R.layout.getting_start_1)
-            val btnsk2: Button = findViewById(R.id.skip_button)
-            btnsk2.setOnClickListener {
-                setContentView(R.layout.getting_start_2)
-            }
-        }
+        setContentView(R.layout.activity_report_crime)
+
     }
 }
