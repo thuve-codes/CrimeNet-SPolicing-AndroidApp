cecfa388a0ab2f0ae68ee395c13ef16be30f1c8e
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-24 19:39:55 +0530

    Fix: Home Activity fixed Done at Train

--- app/src/main/java/com/thuve/crimenet/MainActivity.kt
+++ app/src/main/java/com/thuve/crimenet/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report_crime)
+        setContentView(R.layout.activity_home)
 
     }
 }
