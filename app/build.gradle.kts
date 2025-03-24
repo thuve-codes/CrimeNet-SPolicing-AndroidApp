95d0f14e87ac2fe344140d3b30ef54d2bbdd6474
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-24 18:55:33 +0530

    Fixed: Front end Done at Train

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.cardview)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
